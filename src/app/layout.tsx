83a90845c8e670fb57bc8e82ecf8f489d8d406b2
chrisarevalo11 <christian.arevalo.fonseca@gmail.com>
2025-04-10 08:24:37 -0500

    updated title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import './globals.css';
 import ContextProvider from '@/context'
 
 export const metadata: Metadata = {
-  title: "AppKit in Next.js + wagmi",
-  description: "AppKit example dApp",
+  title: "Rootstock Reown AppKit Example",
+  description: "Rootstock Reown AppKit Example",
 };
 
 export default async function RootLayout({
